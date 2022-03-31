f7e5bf9366bd6ccdb8e423eeb8a911d57dd9cd1a
Reds93 <47994056+Reds93@users.noreply.github.com>
2022-03-31 16:11:54 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Démarrage du container en cours.."
-while ! ping -c 1 $1; do
+while ! ping -c 1 $Container_IP; do
         sleep 1
 done
 
 >/var/jenkins_home/.ssh/known_hosts
 
-scp -o "StrictHostKeyChecking=no" service.sh root@$1:install.sh
-ssh -o "StrictHostKeyChecking=no" root@$1 "chmod 755 install.sh; bash install.sh $2; rm -f install.sh"
+scp -o "StrictHostKeyChecking=no" service.sh root@$Container_IP:install.sh
+ssh -o "StrictHostKeyChecking=no" root@$Container_IP "chmod 755 install.sh; bash install.sh $Container_services; rm -f install.sh"
