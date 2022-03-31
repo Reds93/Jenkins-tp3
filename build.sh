bc15e9ecbce7bf1ca07729feaeb4c5a10bccc7b9
Reds93 <47994056+Reds93@users.noreply.github.com>
2022-03-31 16:21:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 echo "$Container_sshkey_pub" > sshkeypub
 
-scp sshpubkey root@192.168.56.120:sshkeypub
-scp lxcdeploy.sh root@192.168.56.120:lxcdeploy.sh
-ssh root@192.168.56.120 "chmod 755 lxcdeploy.sh; bash lxcdeploy.sh $Container_name $Container_pwd $Container_IP $Container_ID; rm -f lxcdeploy.sh"
+scp sshpubkey root@192.168.56.60:sshkeypub
+scp lxcdeploy.sh root@192.168.56.60:lxcdeploy.sh
+ssh root@192.168.56.60 "chmod 755 lxcdeploy.sh; bash lxcdeploy.sh $Container_name $Container_pwd $Container_IP $Container_ID; rm -f lxcdeploy.sh"
