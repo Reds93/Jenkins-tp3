9bcce30065023e99da956411e3b9a06606819431
Reds93 <47994056+Reds93@users.noreply.github.com>
2022-03-31 15:21:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-print_help() {
+echo "$5" > sshpubkey
+
+scp sshpubkey root@192.168.56.120:sshpubkey
+scp lxcdeploy.sh root@192.168.56.120:lxcdeploy.sh
+ssh root@192.168.56.120 "chmod 755 lxcdeploy.sh; bash lx.sh $1 $2 $3 $4; rm -f lxcdeploy.sh"
